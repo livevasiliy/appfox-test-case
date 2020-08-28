f3ed511b6f41179d181444de29cb51b02d7eb1e7
Vasiliy Pivovarov <vasiliy.mv.obzor@gmail.com>
2020-08-28 10:43:25 +0400

    Добавил в dropdown меню ссылку на home страницу.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="{{ route('home') }}" class="dropdown-item">
+                                        {{ __('Profile') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
